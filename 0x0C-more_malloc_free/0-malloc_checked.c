404cd9bb0f781da5b541d2d79fe1000f10fdb10d
pogechi1875 <ogp1875@gmail.com>
2022-04-07 15:13:56 -0700

    Malloc

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,18 +1,19 @@
-#include <stdlib.h>
 #include "main.h"
-
 /**
- * malloc_checked - allocates memory using malloc
- * @b: size to be allocated
- * Return: pointer to allocated memory or exit with 98
- **/
+ *malloc_checked - array for prints a string
+ *@b: number of memory
+ *Return: void
+ */
+
 void *malloc_checked(unsigned int b)
 {
-	void *p;
+void *p;
 
-	p = malloc(b);
-	if (p == NULL)
-		exit(98);
-	else
-		return (p);
+p = malloc(b);
+if (p == NULL)
+{
+exit(98);
+}
+
+return (p);
 }
--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,41 +1,51 @@
 #include "main.h"
-#include <stdlib.h>
+
 /**
- *string_nconcat - concatenates two strings
- *@s1: pointer to the first string
- *@s2: pointer to the second string
- *@n: number of bytes used fron second string
- *
- *Return: NULL if malloc fails allocating memory
- *        ptr pointer to the concatenated string
+ * string_nconcat - concatenates two strings.
+ * @s1: first string
+ * @s2: second string
+ * @n: index
+ * Return: char pointer
  */
+
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *ptr;
-	unsigned int len1 = 0, len2 = 0, i = 0;
+	char *p;
+	unsigned int size1 = 0, size2 = 0, i;
 
 	if (s1 == NULL)
 		s1 = "";
+
 	if (s2 == NULL)
 		s2 = "";
-	while (s1[len1] || s2[len2])
-	{
-		if (s1[len1])
-			len1++;
-		if (s2[len2])
-			len2++;
-	}
-	len1++;
-	if (n >= len2)
-		n = len2;
-	ptr = malloc((len1 + n) * sizeof(char));
-	if (ptr == NULL)
-		return (NULL);
-	for (i = 0; s1[i]; i++)
-		ptr[i] = s1[i];
-	for (; i - len1 + 1 < n && s2[i - len1 + 1]; i++)
-		ptr[i] = s2[i - len1 + 1];
-	ptr[i] = '\0';
 
-	return (ptr);
-}
+	while (s1[size1] != '\0')
+	{
+		size1++;
+	}
+
+	while (s2[size2] != '\0')
+	{
+		size2++;
+	}
+
+	if (n > size2)
+	n = size2;
+	p = malloc((size1 + n + 1) * sizeof(char));
+
+	if (p == NULL)
+		return (0);
+
+	for (i = 0; i < size1; i++)
+	{
+		p[i] = s1[i];
+	}
+
+	for (; i < (size1 + n); i++)
+	{
+		p[i] = s2[i - size1];
+	}
+	p[i] = '\0';
+
+return (p);
+
